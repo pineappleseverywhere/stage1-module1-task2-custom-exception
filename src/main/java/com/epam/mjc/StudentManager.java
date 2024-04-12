30687d00c260d0aae67833d0f158907c04fd2536
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-12 18:36:11 -0400

    s

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,17 +5,18 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException {
+    Student student = Student.getValueOf(studentID);
+    if (student == null) {
+      throw new StudentNotFoundException(studentID);
+    }
+    return student;
   }
 
-  public static void main(String[] args) {
-    StudentManager manager = new StudentManager();
+  class StudentNotFoundException extends IllegalArgumentException {
 
-    for (int i = 0; i < IDs.length; i++) {
-      Student student = manager.find(IDs[i]);
-      System.out.println("Student name " + student.getName());
+    public StudentNotFoundException(long id) {
+      super("Could not find student with ID " + id);
     }
-
   }
 }
