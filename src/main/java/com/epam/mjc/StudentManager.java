196a4d297c08b181219fac48b644025be31c1e5d
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-12 18:44:53 -0400

    s

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -1,22 +1,21 @@
 package com.epam.mjc;
 
-
 public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) throws StudentNotFoundException {
+  public Student find(long studentID){
     Student student = Student.getValueOf(studentID);
     if (student == null) {
-      throw new StudentNotFoundException(studentID);
+
+      throw new customException(studentID);
     }
     return student;
   }
+}
 
-  class StudentNotFoundException extends IllegalArgumentException {
-
-    public StudentNotFoundException(long id) {
+class customException extends IllegalArgumentException{
+    public customException(long id){
       super("Could not find student with ID " + id);
     }
-  }
 }
